79709bbccb1a2bb2f819b3847799bc7a2b387017
Jan Sladký <sladky.honza@gmail.com>
2024-03-18 19:54:55 +0100

    Uloha - vytvoreni noveho li elementu - zakladni funkce

--- js/legit-app.js
+++ js/legit-app.js
@@ -1,22 +1,14 @@
 //Úloha která mi přidá nový li do seznamu
+function createNewCard(title, text) {
+	//Zatargetování na konkrétní ul který spadá pod element s .cards
+	let cardContainer = document.querySelector('.cards ul');
 
-//Zatargetování na konkrétní ul který spadá pod element s .cards
-let cardContainer = document.querySelector('.cards ul');
+	//vytvořím si nový li element, vložím si do něj nadpis a text
+	let newCard = document.createElement('li');
+	newCard.innerHTML = '<h3>' + title + '</h3><p>' + text + '</p>';
 
-//Najdu si h3 element a text p
-let title = 'Nový nadpis';
-let text = 'A tohle je nový text, který mi vyplní text karty protože jedna věta je moc málo.';
+	//Vložím do rodiče
+	cardContainer.appendChild(newCard);
+}
 
-//vytvořím si nový li element, vložím si do něj nadpis a text
-let newCard = document.createElement('li');
-newCard.innerHTML = '<h3>' + title + '</h3><p>' + text + '</p>';
-
-//Alternativní zápis do více řádků
-newCard.innerHTML = `
-<h3> ${title} </h3>
-<p> 
-    ${text} 
-</p>
-`
-//Vložím do rodiče
-cardContainer.appendChild(newCard);
+createNewCard('Toto je první nadpis', 'A toto je text karty a musí být delší než jedna věta')
