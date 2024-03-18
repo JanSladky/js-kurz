ff619900cbe5c8748655b080a46d7bf55e88d17c
Jan Sladký <sladky.honza@gmail.com>
2024-03-18 18:41:01 +0100

    Uloha H - funkce na formatovani meny a ruznych men a vypsani do konzole

--- js/legit-app.js
+++ js/legit-app.js
@@ -3,8 +3,33 @@ function plus(a, b) {
 	return a + b;
 }
 //Number je lepší než parseInt. parseInt odsekne desetiná čísla, parseFloat je taky možnost
-let firstNumber = Number(prompt('Zadej první číslo:'));
+/* let firstNumber = Number(prompt('Zadej první číslo:'));
 let secondNumber = Number(prompt('Zadej druhé číslo:'));
 
 //Vypíše se do konzole výsledek
-console.log(plus(firstNumber, secondNumber));
+console.log(plus(firstNumber, secondNumber)); */
+
+//Úloha H - funkce která naformátuje jako české koruny, s možností formátovat i v jiných měnách
+function financial(number, currency) {
+	if (currency === 'usd') {
+		number = number.toLocaleString('en-US', {
+			style: 'currency',
+			currency: 'USD',
+		});
+	} else if (currency === 'sk') {
+		number = number.toLocaleString('sk-SK', {
+			style: 'currency',
+			currency: 'SK',
+		});
+	}
+	 else if (currency === 'czk') {
+		number = number.toLocaleString('cs-CZ', {
+			style: 'currency',
+			currency: 'CZK',
+		});
+	}
+
+	return number;
+}
+
+console.log(financial(123.5454, 'usd'));
