7d2fb3eddc428297d209ec8694732473ac870168
Jan Sladký <sladky.honza@gmail.com>
2024-03-18 18:42:36 +0100

    Uloha H - uprava funkce a pridani defaultni hodnoty kdyz nezadam menu tak to naformatuje v czk

--- js/legit-app.js
+++ js/legit-app.js
@@ -10,7 +10,8 @@ let secondNumber = Number(prompt('Zadej druhé číslo:'));
 console.log(plus(firstNumber, secondNumber)); */
 
 //Úloha H - funkce která naformátuje jako české koruny, s možností formátovat i v jiných měnách
-function financial(number, currency) {
+//Úprava funkce kdy přednastavím nějakou hodnotu třeba CZK
+function financial(number, currency = 'czk') {
 	if (currency === 'usd') {
 		number = number.toLocaleString('en-US', {
 			style: 'currency',
