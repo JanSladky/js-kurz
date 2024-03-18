e067f60578c5da5f73e1a39d5200984cfdf177db
Jan Sladký <sladky.honza@gmail.com>
2024-03-18 19:45:12 +0100

    Uloha - pridani noveho li elementu do html  - s ukazkou alternativniho zapisu do vice radku

--- js/legit-app.js
+++ js/legit-app.js
@@ -1,35 +1,22 @@
-//Úloha D - Funkce která přijimá 2 argumenty, spočítá čísla a vrátí zpět do programu
-function plus(a, b) {
-	return a + b;
-}
-//Number je lepší než parseInt. parseInt odsekne desetiná čísla, parseFloat je taky možnost
-/* let firstNumber = Number(prompt('Zadej první číslo:'));
-let secondNumber = Number(prompt('Zadej druhé číslo:'));
+//Úloha která mi přidá nový li do seznamu
 
-//Vypíše se do konzole výsledek
-console.log(plus(firstNumber, secondNumber)); */
+//Zatargetování na konkrétní ul který spadá pod element s .cards
+let cardContainer = document.querySelector('.cards ul');
 
-//Úloha H - funkce která naformátuje jako české koruny, s možností formátovat i v jiných měnách
-//Úprava funkce kdy přednastavím nějakou hodnotu třeba CZK
-function financial(number, currency = 'czk') {
-	if (currency === 'usd') {
-		number = number.toLocaleString('en-US', {
-			style: 'currency',
-			currency: 'USD',
-		});
-	} else if (currency === 'sk') {
-		number = number.toLocaleString('sk-SK', {
-			style: 'currency',
-			currency: 'SK',
-		});
-	} else {
-		number = number.toLocaleString('cs-CZ', {
-			style: 'currency',
-			currency: 'CZK',
-		});
-	}
+//Najdu si h3 element a text p
+let title = 'Nový nadpis';
+let text = 'A tohle je nový text, který mi vyplní text karty protože jedna věta je moc málo.';
 
-	return number;
-}
+//vytvořím si nový li element, vložím si do něj nadpis a text
+let newCard = document.createElement('li');
+newCard.innerHTML = '<h3>' + title + '</h3><p>' + text + '</p>';
 
-console.log(financial(123.5454));
+//Alternativní zápis do více řádků
+newCard.innerHTML = `
+<h3> ${title} </h3>
+<p> 
+    ${text} 
+</p>
+`
+//Vložím do rodiče
+cardContainer.appendChild(newCard);
